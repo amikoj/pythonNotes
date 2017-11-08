d765d45c504a03fcc6e0b97ef4e23bf7c580554b
hfcai <caihaifei@enjoytoday.cn>
2017-11-08 23:52:18 +0800

    add spider

---
+++ spider/__init__.py
@@ -0,0 +1,6 @@
+# !/usr/bin/env python
+# -*- encoding:utf-8 -*-
+
+
+if __name__=="__main__":
+    print "用于python爬虫学习记录."
---
+++ spider/url.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+# -*-encoding:utf-8 -*-
+
+import sys,urllib,urllib2
+
+
+'''
+urllib和urllib2的使用
+'''
+
+def main():
+    '''
+    :return: 
+    '''
+
+
+
+
+
